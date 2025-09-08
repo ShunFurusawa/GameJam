0a37ff715cdff1f45ff8034d32aac1251ec49e6a
furu_shun1113 <114386792+ShunFurusawa@users.noreply.github.com>
2025-09-08 15:30:53 +0900

    カーソル仮実装

---
+++ GameJam/Assets/Scripts/Furusawa/Bullet.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Scripts.Furusawa
+{
+    public class Bullet : MonoBehaviour
+    {
+        // 画面外に出たら消す
+        public void OnBecameInvisible()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ GameJam/Assets/Scripts/Furusawa/Shooter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Scripts.Furusawa
+{
+    public class Shooter : MonoBehaviour
+    {
+        [SerializeField] private float power = 100f;
+        [SerializeField] private float shootInterval = 0.5f;
+        [SerializeField] GameObject bullet;
+        
+        private float timer = 0f;
+
+        private void Update()
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= shootInterval)
+            {
+                Shoot();
+            }
+        }
+        
+        private void Shoot()
+        {
+            GameObject newBullet = Instantiate(bullet, transform.position, transform.rotation);
+            Rigidbody2D rb = newBullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(transform.up * power);
+            timer = 0f;
+        }
+        
+    }
+}
