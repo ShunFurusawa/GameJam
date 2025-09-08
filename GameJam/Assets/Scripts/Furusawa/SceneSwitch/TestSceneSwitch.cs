9b00a6fd1d04e2f1860933335f2a267d48c41403
furu_shun1113 <114386792+ShunFurusawa@users.noreply.github.com>
2025-09-08 16:07:13 +0900

    シーン切り替え実装

---
+++ GameJam/Assets/Scripts/Furusawa/SceneSwitch/TestSceneSwitch.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+namespace Scripts.Furusawa.SceneSwitch
+{
+    public class TestSceneSwitch : MonoBehaviour
+    {
+        [SerializeField] private FadeAndSceneTransition fadeAndSceneTransition;
+        
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                fadeAndSceneTransition.StartTransition(); // 遷移先のシーン名を指定
+            }
+        }
+    }
+}
