e83a79fbbb07dba0370ee931baebf58fa7972b79
furu_shun1113 <114386792+ShunFurusawa@users.noreply.github.com>
2025-09-09 12:32:37 +0900

    Jointだめかも

---
+++ GameJam/Assets/Scripts/Furusawa/FollowTarget.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(TargetJoint2D))]
+public class FollowTarget : MonoBehaviour
+{
+    [SerializeField] private Transform target; 
+    
+    [SerializeField] private TargetJoint2D targetJoint;
+    
+
+    void Update()
+    {
+       
+        if (target != null)
+        {
+            targetJoint.target = target.position;
+        }
+    }
+}
