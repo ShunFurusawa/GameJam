4b5029b0584a3199794491841a0bf1702901339e
aoi0308-netizen <aoi0308@outlook.jp>
2025-09-09 10:29:43 +0900

    スコア計算と表示(途中)

---
+++ GameJam/Assets/Scripts/Hatakeyama/ScoreManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using TMPro;
+public static class ScoreManager
+{
+    public static int FinalScore = 0;           //最終スコア
+    public static float RemainTime = 0; // 残り時間を保持
+
+
+    public static void SaveScore(int score)     //スコア保存
+    {
+        FinalScore = score;
+        Debug.Log("Score Saved: " + FinalScore);
+    }
+
+    
+    public static int GetScore()                //スコア習得
+    {
+        return FinalScore;
+    }
+    public static void ShowScore(TextMeshProUGUI scoreText)     //スコア表示
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: " + FinalScore;
+        }
+        else
+        {
+            Debug.LogWarning("Score Text is not assigned");
+        }
+    }
+}
