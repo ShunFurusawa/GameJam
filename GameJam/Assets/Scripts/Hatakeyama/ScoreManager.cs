552775eeedfc15d5829185ea8e69e9e48112d8ba
aoi0308-netizen <aoi0308@outlook.jp>
2025-09-09 11:24:29 +0900

    フォント修正

--- GameJam/Assets/Scripts/Hatakeyama/ScoreManager.cs
+++ GameJam/Assets/Scripts/Hatakeyama/ScoreManager.cs
@@ -21,7 +21,7 @@ public static class ScoreManager
     {
         if (scoreText != null)
         {
-            scoreText.text = "Score: " + FinalScore;
+            scoreText.text = "" + FinalScore;
         }
         else
         {
